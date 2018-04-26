658cba4dc8e8f106158163a47bcc06cba26d7819
zlfpage <897418436@qq.com>
2018-04-26 17:54:36 +0800

    add total money

---
+++ src/const/myMoney.js
@@ -0,0 +1,32 @@
+export const myMone = [
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon glyphicon-credit-card' 
+    },
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon glyphicon-usd'
+    },
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon glyphicon-tower'
+    },
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon  glyphicon-signal'
+    },
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon glyphicon-globe'
+    },
+    {
+        type: 'card',
+        count: '26000',  
+        icon: 'glyphicon glyphicon-transfer'
+    },
+]
