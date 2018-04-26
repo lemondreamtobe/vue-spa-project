1cfc5f52e4a61bd813c00cf978b7252c2b123b83
Lemon_zhang <897418436@qq.com>
2018-04-26 23:04:04 +0800

    add bootstrap

--- src/const/myMoney.js
+++ src/const/myMoney.js
@@ -1,32 +1,34 @@
-export const myMone = [
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon glyphicon-credit-card' 
-    },
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon glyphicon-usd'
-    },
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon glyphicon-tower'
-    },
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon  glyphicon-signal'
-    },
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon glyphicon-globe'
-    },
-    {
-        type: 'card',
-        count: '26000',  
-        icon: 'glyphicon glyphicon-transfer'
-    },
-]
+let myMoney = [{
+		type: '银行卡',
+		count: '26000',
+		icon: 'glyphicon glyphicon-credit-card'
+	},
+	{
+		type: '现金',
+		count: '150',
+		icon: 'glyphicon glyphicon-usd'
+	},
+	{
+		type: '羊城通',
+		count: '20',
+		icon: 'glyphicon glyphicon-tower'
+	},
+	{
+		type: '收益占比',
+		count: '0%',
+		icon: 'glyphicon glyphicon-globe'
+	},
+	{
+		type: '开支总差',
+		count: '0',
+		icon: 'glyphicon glyphicon-transfer'
+	},
+];
+myMoney.splice(2, 0, {
+	type: '总资产',
+	count: (+myMoney[0].count) + (+myMoney[1].count) + (+myMoney[2].count),
+	icon: 'glyphicon  glyphicon-signal'
+});
+export {
+	myMoney
+}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import $ from 'jquery'
+import './assets/css/bootstrap.min.css'
+import './assets/js/bootstrap.min'
 // 引用API文件
 import api from './api/index.js'
 // 将API方法绑定到全局
@@ -19,10 +21,10 @@ Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: {
-    App
-  }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: {
+		App
+	}
 })
