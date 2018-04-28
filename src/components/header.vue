20a31aa08366354261b95e0b3e5fdb2af5c6e2f8
GM-03-01-0296\kinkoo <897418436@qq.com>
2018-04-28 15:26:20 +0800

    fix height x2

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <h1 class="logo">
       <a class="logo-img"><img src="../../static/img/logo.png"></a>
-      <a class="logo-title" href="/index#/index">Vue Test Demo</a>
+      <a class="logo-title" href="/">Vue Test Demo</a>
     </h1>
   </header>
 </template>
