e7c8ff3bd42c1b2c8bf864b17f2d9df923163de6
lemondreamtobe <897418436@qq.com>
2018-05-15 14:17:16 +0800

    第三方登录的图片hover处理

--- src/components/thirdlogin.vue
+++ src/components/thirdlogin.vue
@@ -1,9 +1,30 @@
 <template>
     <div id="thirdlogin">
-        <img src="../../static/img/me1.jpg" class="third-icon qq" >
-        <img src="../../static/img/me2.jpg" class="third-icon wechat" >
+        <img src="../../static/img/me1.jpg" class="third-icon qq">
+        <img src="../../static/img/me2.jpg" class="third-icon wechat">
+        <el-dialog :visible.sync="imgview"></el-dialog>
     </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {     
+        imgview: false,
+      };
+    },
+    created() {
+
+    },
+    methods: {
+      overShow() {
+          this.imgview = true;
+      },
+      overHide() {
+        this.imgview = false;
+      }
+    },
+  };
+</script>
 <style lang="scss" scoped>
 #thirdlogin {
   display: flex;
@@ -20,12 +41,21 @@
     border-width: 2px;
     border-radius: 5px;
     text-align: center;
+     transition: 0.5s all;
   }
   & > .third-icon.qq {
     border-color: #409eff;
   }
   & > .third-icon.wechat {
     border-color: #67c23a;
+  }
+  & > .third-icon.qq:hover {
+      width: 230px;
+      height: 230px;
+  }
+  & > .third-icon.wechat:hover {
+     width: 230px;
+      height: 230px;
   }
 }
 </style>
