25b8aff2419b07570f3d32a8ac9f6b658d3114b9
lemondreamtobe <897418436@qq.com>
2018-06-07 11:16:06 +0800

    增加备案说明

--- src/page/login.vue
+++ src/page/login.vue
@@ -3,8 +3,9 @@
        
         <el-row type="flex" class="row-bg" justify="center">
             <el-col :span="6">
-                <div class="grid-content bg-purple">               
-                    <h3 class="login-head">用户登录</h3>
+                <div class="grid-content bg-purple">
+                                   
+                    <h3 class="login-head"><img src="../../static/img/mepic.jpg" style="width:61px;height:48px;float:left">声明:本项目是本网站作者博客所用,不属于企业/商用网站</h3>
                 </div>
                 <div class="login-tab">
                     <el-tabs v-model="activeName" @tab-click="handleClick">
